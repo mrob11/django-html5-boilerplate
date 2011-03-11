d8e0cea771dd665bfc4d5fdde1e3849335390c0e
Mike Robinson <mike.robinson.81@gmail.com>
2011-03-11 14:28:18 -0500

    Fixed a pretty stupid bug in the base.html

--- projectname/urls.py
+++ projectname/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # (r'^bonefish/', include('bonefish.foo.urls')),
+    # (r'^myapp/', include('myapp.urls')),
 
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
