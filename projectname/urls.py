435262eb055db764df332021f15150338283c586
Mike Robinson <mike.robinson.81@gmail.com>
2011-10-28 12:54:44 -0400

    Added url for root home page view

--- projectname/urls.py
+++ projectname/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 admin.autodiscover()
+
+
 
 urlpatterns = patterns('',
     # url(r'^myapp/', include('myapp.urls')),
     
+    # Home Page -- Replace if you like
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
